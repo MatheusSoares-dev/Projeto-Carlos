4da6d73337a556d4dd9af8522cdba125f7768179
Matheus <matheus.loc.soares@gmail.com>
2026-07-13 18:35:28 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const telefone = '5531987097928'
+
+function servico(botao) {
+    const servico = botao.id
+    const mensagem = `Olá, estava interessado no serviço de ${servico}, podemos fazer um orçamento?`
+
+    const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`
+    window.open(url,"_blank")
+}
+
+function enviarWhats(event) {
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const msg = document.getElementById('mensagem').value;
+
+    const text = `Olá tudo bom? meu nome e ${nome}, ${msg}`
+    const url = `https://wa.me/${telefone}?text=${encodeURIComponent(text)}`
+    window.open(url,"_blank")
+}
